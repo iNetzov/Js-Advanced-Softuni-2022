15ade6386d517b8de479c243f88615aff7945bfd
iNetzov <ivn9812@gmail.com>
2022-09-17 12:52:32 +0300

    +Syntax Lab:   -Task No2 String Lenght   -change Echo Function location

---
+++ Syntax Lab/StringLength.js
@@ -0,0 +1,7 @@
+function prossesText(paramOne, paramTwo, paramThree){
+
+    console.log((paramOne.length + paramTwo.length + paramThree.length))
+    console.log(Math.floor((paramOne.length + paramTwo.length + paramThree.length)/3))
+}
+
+prossesText('Chocolate','ice cream','cake')
